1aeae8d95d458429ff2b6bca73cbecc8e93ea9a1
matevzStinjek <stinjek@gmail.com>
2020-05-03 22:50:11 +0200

    linter compliance changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,9 @@
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
         </div>
-        <router-view/>
+        <div />
+        <img>
+        <router-view />
     </div>
 </template>
 
--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -37,7 +37,7 @@
 export default {
     name: 'HelloWorld',
     props: {
-        msg: String,
+        msg: { type: String, required: true },
     },
 }
 </script>
