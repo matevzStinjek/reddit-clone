f9ef326b00405ce2795578e26683236be20b173d
Denis Rajlic <denis.rajlic@gmail.com>
2020-06-17 15:51:04 +0200

    add 4 space indent and remove 95% width restriction

--- client/src/modules/subreddit/components/CreatePost.vue
+++ client/src/modules/subreddit/components/CreatePost.vue
@@ -10,31 +10,30 @@
 @import 'shared/styles/colors.scss';
 
 .component-wrapper {
-  display: flex;
-  background: $void;
-  padding: 8px;
+    display: flex;
+    background: $void;
+    padding: 8px;
 
-  &__avatar {
-    max-width: 100%;
-    max-height: 32px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-
-  &__search {
-    width: 95%;
-    background: $ink;
-  }
-
-  &__link {
-    margin-left: 8px;
-    color: $white;
-    font-size: 40px;
-
-    &:hover {
-      background: $slate;
-      cursor: pointer;
+    &__avatar {
+      max-width: 100%;
+      max-height: 32px;
+      border-radius: 50%;
+      margin-right: 8px;
     }
-  }
+
+    &__search {
+      background: $ink;
+    }
+
+    &__link {
+      margin-left: 8px;
+      color: $white;
+      font-size: 40px;
+
+      &:hover {
+        background: $slate;
+        cursor: pointer;
+      }
+    }
 }
 </style>
