939dbb08573ffa731bff2c0c26983b428bbbb849
Denis Rajlic <denis.rajlic@gmail.com>
2020-06-24 18:09:04 +0200

    get community info from the constants.json file & from the mock data

--- client/src/modules/subreddit/store/subreddit.store.js
+++ client/src/modules/subreddit/store/subreddit.store.js
@@ -5,15 +5,20 @@ export default {
     state: {
         subredditInfo: null,
         isUserJoined: null,
+        communityConstants: null,
+        communityInfo: null,
     },
     getters: {
         navbarItems: () => constants.navbarOptions,
         isUserJoined: state => state.isUserJoined,
         info: state => state.subredditInfo,
+        communityConstants: () => constants.communityConstants,
+        communityInfo: state => state.communityInfo,
     },
     mutations: {
         SET_JOINED: (state, data) => state.isUserJoined = data,
         SET_SUBREDDIT_INFO: (state, data) => state.subredditInfo = data,
+        SET_COMMUNITY: (state, data) => state.communityInfo = data,
     },
     actions: {
         initialise ({ commit }) {
@@ -22,6 +27,9 @@ export default {
                 .catch(err => console.log(err))
             service.fetchJoinedStatus()
                 .then(res => commit('SET_JOINED', res))
+                .catch(err => console.log(err))
+            service.fetchCommunityInfo()
+                .then(res => commit('SET_COMMUNITY', res))
                 .catch(err => console.log(err))
         },
         async toggleJoinedStatus ({ commit }) {
--- client/src/modules/subreddit/views/SubredditView.vue
+++ client/src/modules/subreddit/views/SubredditView.vue
@@ -1,30 +1,16 @@
 <template>
     <div>
         <subreddit-header />
-        <div class="my-grid">
-            <input type="text">
-            <about-community />
-        </div>
     </div>
 </template>
 
 <script>
 import SubredditHeader from 'subreddit/components/SubredditHeader.vue'
-import AboutCommunity from 'shared/components/stateless/AboutCommunity.vue'
 
 export default {
     name: 'Subreddit',
     components: {
         SubredditHeader,
-        AboutCommunity,
     },
 }
 </script>
-
-<style scoped lang="scss">
-
-.my-grid {
-    display: grid;
-    grid-template-columns: 4fr 1fr;
-}
-</style>
