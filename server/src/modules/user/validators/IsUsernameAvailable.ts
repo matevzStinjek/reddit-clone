5617b2d8c2ca1bfc78c157610d41af5f8fc04732
matevzStinjek <stinjek@gmail.com>
2021-03-31 13:58:43 +0200

    style(user): whitespace

--- server/src/modules/user/validators/IsUsernameAvailable.ts
+++ server/src/modules/user/validators/IsUsernameAvailable.ts
@@ -1,9 +1,4 @@
-import {
-    registerDecorator,
-    ValidationOptions,
-    ValidatorConstraint,
-    ValidatorConstraintInterface,
-} from "class-validator";
+import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from "class-validator";
 import { UserService } from "../service";
 
 @ValidatorConstraint({ async: true })
