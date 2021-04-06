6b33689a10c34a36d2c8b87a53deda86d419e20e
matevzStinjek <stinjek@gmail.com>
2021-04-06 16:46:50 +0200

    refactor: order imports by path

--- server/src/models.ts
+++ server/src/models.ts
@@ -1,4 +1,4 @@
-export { User } from "modules/user/models/User";
-export { Guest } from "modules/user/models/Guest";
 export { Post } from "modules/post/models/Post";
 export { Subreddit } from "modules/subreddit/models/Subreddit";
+export { Guest } from "modules/user/models/Guest";
+export { User } from "modules/user/models/User";
--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,9 +1,9 @@
-import { UserResolver } from "modules/user/resolvers/UserResolver";
 import { PostResolver } from "modules/post/resolvers/PostResolver";
 import { SubredditResolver } from "modules/subreddit/resolvers/SubredditResolver";
+import { UserResolver } from "modules/user/resolvers/UserResolver";
 
 export const resolvers = [
-    UserResolver,
     PostResolver,
     SubredditResolver,
+    UserResolver,
 ] as const;
